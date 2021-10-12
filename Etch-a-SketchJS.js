60576abafc6882759b744b7c97193ac48a9952f2
Zach-Adcock <daespnman@gmail.com>
2021-10-11 19:53:01 -0700

    comment

--- Etch-a-SketchJS.js
+++ Etch-a-SketchJS.js
@@ -15,6 +15,7 @@ const getCurrentColor = () => {
     })
     switch (currentButton) {
         case 'Color Selector':
+            clearButton.innerText="efg"
             return colorSelectorCircle.value
             break;
         case 'Ludicrous Mode':
@@ -119,7 +120,7 @@ gridContainer.addEventListener('touchmove', function(e) {
     clearButton.innerText="e"
     let cellLocation = e.changedTouches[0];
     let cellTarget = document.elementFromPoint(cellLocation.clientX, cellLocation.clientY);
-    if (cellTarget.classList.contains('cell')) cellTarget.style.background = getCurrentColor()
+    if (cellTarget.classList.contains('cell')) clearButton.innerText="ef"//cellTarget.style.background = getCurrentColor()
 });
 
 createGrid(); //initial grid creation. Default size used
